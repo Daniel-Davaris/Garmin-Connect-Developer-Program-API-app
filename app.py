8063a24f9d899870bc346b51c555724a522d9436
Daniel Davaris <danieldavaris@outlook.com.au>
2023-04-21 15:51:37 +1000

    Working code to generate user authorization link

--- app.py
+++ app.py
@@ -1,32 +1,75 @@
 import requests
-from requests_oauthlib import OAuth2Session
+import hmac
+import hashlib
+import time
+import random
+import base64
+import urllib.parse
 
-# Sep up the OAuth 2.0 client with your client ID and client secret
-from config import * # Stored in git ingnored file for security
+# Consumer key and secret
+from config import *
 
-# Setup the OAuth 2.0 session 
-garmin_oauth = OAuth2Session(client_id,redirect_uri='http://localhost:8000/callback')
+# API endpoint URL
+url = 'https://connectapi.garmin.com/oauth-service/oauth/request_token'
+print("\nRequest URL:\n",url)
 
-# A function to initate the OAuth 2.0 flow and redirect the user to the Garmin Connect login page
-def authorise():
-    # Generate the authorisation URL with the requested data scopes
-    authorisation_url, state = garmin_oauth.authorization_url(
-        authorisation_base_url,
-        scope=['read_all','profile'],
-    )
+# OAuth parameters
+params = {
+    'oauth_consumer_key': consumer_key,
+    'oauth_nonce': str(random.randint(0, 1e16)),
+    'oauth_signature_method': 'HMAC-SHA1',
+    'oauth_timestamp': str(int(time.time())),
+    'oauth_version': '1.0',
+}
 
-    # Redirect the user to the authorization URL
-    return authorisation_url
+# Generate signature base string
+base_string = '&'.join([
+    'POST',
+    requests.utils.quote(url, safe=''),
+    requests.utils.quote('&'.join([
+        f"{requests.utils.quote(k, safe='')}"
+        f"={requests.utils.quote(params[k], safe='')}"
+        for k in sorted(params)
+    ]), safe='')
+])
+print("\nSignature Base String: \n", base_string)
 
-# A function to handle the callback URL after the user logs in and authorizes the app
-def callback(url):
-    # Exchange the authorization code for an access token
-    garmin_oauth.fetch_token(
-        token_url,
-        authorization_response=url,
-        client_secret=client_secret,
-    )
+# Generate signature
+key = f"{requests.utils.quote(consumer_secret, safe='')}&"
+signature = hmac.new(
+    key.encode('utf-8'),
+    base_string.encode('utf-8'),
+    hashlib.sha1
+).digest()
+params['oauth_signature'] = base64.b64encode(signature).decode('utf-8')
+print("\nOAuth signature:\n",key)
+print("\nOAuth signature encoded:\n",params['oauth_signature'])
 
-    # Use the access token to make API requests to the Garmin Connect API
-    access_token = garmin_oauth.token(['access_token'])
-    # Make API requests with the access_token
+# Set headers
+headers = {
+    'Authorization': 'OAuth ' + ', '.join([
+        f"{k}=\"{requests.utils.quote(params[k], safe='')}\","
+        for k in sorted(params)
+    ])[:-1]
+}
+print("\nAuthorization HTTP header:\n",headers)
+
+# Make API request
+response = requests.post(url, headers=headers)
+print("\nRequest response:\n",response)
+
+# Extract request token and secret from response
+response_text = response.text
+request_token = response_text.split('&')[0].split('=')[1]
+request_token_secret = response_text.split('&')[1].split('=')[1]
+
+print(f"\nRequest Token:\n {request_token}")
+print(f"\nRequest Token Secret:\n {request_token_secret}")
+
+
+callback_url = "https://www.opendataconnect.com"
+
+authorize_url = f"https://connect.garmin.com/oauthConfirm?oauth_token={request_token}&oauth_callback={urllib.parse.quote(callback_url)}"
+print("\nUser authorization link:\n",authorize_url,"\n")
+
+

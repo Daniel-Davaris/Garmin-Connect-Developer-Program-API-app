3617a073b75fb84f8a20dd575657d060241139d5
Daniel Davaris <danieldavaris@outlook.com.au>
2023-04-23 13:24:15 +1000

    Temorarily disabling sleeps endpoint to test user access token generation

--- app.py
+++ app.py
@@ -130,8 +130,10 @@ def get_access_token():
     session['access_token'] = access_token
     session['access_token_secret'] = access_token_secret
 
-    return redirect('/sleeps')
-   # return f"Access Token: {access_token}<br>Access Token Secret: {access_token_secret}"
+    print("\naccess_token:\n",access_token)
+    print("\naccess_token_secret:\n",access_token_secret)
+    #return redirect('/sleeps')
+    return f"Access Token: {access_token}<br>Access Token Secret: {access_token_secret}"
 
 
 @app.route("/sleeps")
